225d0325db12e643f0ec2f395084cce0f74308f1
Johan <usuario@AirdeJonBlandon.domain.name>
2022-02-11 16:44:47 -0500

    The file update 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - the function print the alphabet in minus
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 97 ; c < 123 ; c++)
+	{
+		if (c == 101 || c == 113)
+			c++;
+		putchar(c);
+	}
+	putchar(10);
+	return (0);
+}
