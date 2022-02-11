48a0dec231f3bfd6152f59ce537376a78ff81bc0
Johan <usuario@AirdeJonBlandon.domain.name>
2022-02-11 16:50:12 -0500

    The file update 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - the function print the digit nums
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	
+	for (n = 0 ; n < 10 ; n++)
+		printf("%i", n);
+	printf("\n");
+	return (0);
+}
