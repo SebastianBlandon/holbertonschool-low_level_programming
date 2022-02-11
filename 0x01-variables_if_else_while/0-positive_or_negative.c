690c6f3871fc02d82cb6fabb7b8b153fa7ea1218
Johan <usuario@AirdeJonBlandon.domain.name>
2022-02-11 15:19:51 -0500

    The file uodate 0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/*
+/**
  * main - n is a random number and the function find if n is positive
  * negative or zero
  * Return: Always (0) 
