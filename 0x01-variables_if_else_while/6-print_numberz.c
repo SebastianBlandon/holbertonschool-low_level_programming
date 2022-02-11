c290ca79fb31e3c6fd90ec005da1b2fd67e23c59
Johan <usuario@AirdeJonBlandon.domain.name>
2022-02-11 16:58:30 -0500

    The file update 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - the function print the alphabet in minus
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48 ; n < 58 ; n++)
+		putchar(n);
+	putchar(10);
+	return (0);
+}
