fdb51c2ee9b4aa9a9577ab3679d7091c91b781c0
Johan <usuario@MacBook-Air-de-Johan-Blandon.local>
2022-02-15 12:23:05 -0500

    This file contain code about 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_last_digit - Print last digit of the input.
+ * @n: int type intiger
+ * Return: 0 always
+ */
+int print_last_digit(int n)
+{
+	return _putchar(n%10);
+}
