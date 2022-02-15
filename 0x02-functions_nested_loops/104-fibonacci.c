1241c6ea9e8c840488ac53ef3cd57f92776597ce
Johan <usuario@MacBook-Air-de-Johan-Blandon.local>
2022-02-15 18:26:39 -0500

    This file contain code about 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define LARGEST 10000000000
 /**
  * main - the function print fibonacci 50
  * Return: void function
@@ -6,16 +7,31 @@
 int main(void)
 {
 	int i;
-	unsigned long int a = 0, b = 1, sum = 0;
+	unsigned long a = 0, b = 1, save1, save2, sum = 0;
+	unsigned long tmp1, tmp2, tmp3;
 
 	for (i = 0; i < 97; i++)
 	{
-		sum = a + b;
-		printf("%lu", sum);
+		if (a + b > LARGEST)
+		{
+			tmp1 = (a + b) / LARGEST;
+			tmp2 = (a + b) % LARGEST;
+			tmp3 = save1 + save2 + tmp1;
+			save1 = save2;
+			save2 = tmp3;
+			a = b;
+			b = tmp2;
+			printf("%lu%010lu", save2, b);
+		}
+		else
+		{
+			sum = a + b;
+			a = b;
+			b = sum;
+			printf("%lu", sum);
+		}
 		if (i != 96)
 			printf(", ");
-		a = b;
-		b = sum;
 	}
 	printf("\n");
 	return (0);
