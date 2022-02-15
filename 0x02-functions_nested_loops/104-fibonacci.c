d81657f28d38c3181919194c32b496baaa0df2b0
Johan <usuario@MacBook-Air-de-Johan-Blandon.local>
2022-02-15 17:59:17 -0500

    This file contain code about 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,11 +8,11 @@ int main(void)
 	int i;
 	unsigned long a = 0, b = 1, sum = 0;
 
-	for (i = 0; i < 95; i++)
+	for (i = 0; i < 85; i++)
 	{
 		sum = a + b;
 		printf("%lu", sum);
-		if (i != 94)
+		if (i != 84)
 			printf(", ");
 		a = b;
 		b = sum;
