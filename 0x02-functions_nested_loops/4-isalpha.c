8beb49d438a0d98a7ef2ff2112b9e077e61f9cf8
Johan <usuario@MacBook-Air-de-Johan-Blandon.local>
2022-02-15 10:05:28 -0500

    This file contain code about 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * @c: char type letter
  * Return: 1 if lowercase, 0 if not lowercase
  */
-int _islower(int c)
+int _isalpha(int c)
 {
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 		return (1);
