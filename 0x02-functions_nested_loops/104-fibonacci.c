238ddefaa516e276a81ea4a9c3ad68042cce4a4d
Johan <usuario@MacBook-Air-de-Johan-Blandon.local>
2022-02-15 17:53:40 -0500

    This file contain code about 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - the function print fibonacci 50
+ * Return: void function
+ */
+int main(void)
+{
+	int i;
+	unsigned long a = 0, b = 1, sum = 0;
+
+	for (i = 0; i < 98; i++)
+	{
+		sum = a + b;
+		printf("%lu", sum);
+		if (i != 97)
+			printf(", ");
+		a = b;
+		b = sum;
+	}
+	printf("\n");
+	return (0);
+}
+
