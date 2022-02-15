fe593a40763132cc337de34fae32faed8185e7fb
Johan <usuario@MacBook-Air-de-Johan-Blandon.local>
2022-02-15 15:40:49 -0500

    This file contain code about 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,7 @@ void print_times_table(int n)
 {
 	int n1, n2;
 
-	if (n <= 15 && n >= 1)
+	if (n >= 1 && n <=15)
 	{
 		for (n1 = 0; n1 <= n; n1++)
 		{
@@ -30,6 +30,4 @@ void print_times_table(int n)
 			printf("\n");
 		}
 	}
-	else if (n == 0)
-		
 }
