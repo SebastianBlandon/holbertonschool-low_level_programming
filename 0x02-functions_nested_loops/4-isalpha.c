69b8e9a7e2cdc2a532686b35c8894f813fd0b4f7
Johan <usuario@MacBook-Air-de-Johan-Blandon.local>
2022-02-15 10:07:51 -0500

    This file contain code about 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _islower - Return 1 if letter is alphebetic char, 0 if not.
+ * _isalpha - Return 1 if letter is alphebetic char, 0 if not.
  * @c: char type letter
  * Return: 1 if lowercase, 0 if not lowercase
  */
