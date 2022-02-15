4698fa91f2e31c9b06d023640b19ccb8087fe836
Johan <usuario@MacBook-Air-de-Johan-Blandon.local>
2022-02-15 15:57:36 -0500

    This file contain code about 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - the function print all multiples of 3 and 5 below 1024
+ * Return: void function
+ */
+int main(void)
+{
+	int i = 0, sum = 0;
+	
+	do
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+		i++;
+	}while (i < 1024);
+	printf("%i\n", sum);
+	return (0);
+}
+
