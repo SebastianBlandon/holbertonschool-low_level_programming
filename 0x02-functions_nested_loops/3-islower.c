75aa7ffc3660c1fdd4a1912f084f72c6997c9f1c
Johan <usuario@MacBook-Air-de-Johan-Blandon.local>
2022-02-15 09:48:34 -0500

    This file contain code about 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
-#include "main.h"
+o#include "main.h"
 /**
  * main - the function print char without stdin
  * Return: 0
  */
 int _islower(int c)
 {
-	if(c >= 65 || c <= 90)
+	if(c >= 65 && c <= 90)
 		return (0);
-	if(c >= 97 || c <= 122)
+	if(c >= 97 && c <= 122)
 		return 1;
 }
