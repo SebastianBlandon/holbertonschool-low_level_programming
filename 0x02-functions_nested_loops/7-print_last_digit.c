663cfcd295ccfeb498167d6641685ee7d45da7ee
Johan <usuario@MacBook-Air-de-Johan-Blandon.local>
2022-02-15 12:47:33 -0500

    This file contain code about 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,15 +7,16 @@
 int print_last_digit(int n)
 {
 	int n1;
+	
 	if (n < 0)
 	{
-		n1 = -1 * (n%10);
-		_putchar(48+n1);
+		n1 = - 1 * (n % 10);
+		_putchar(48 + n1);
 		return (n1);	
 	}
 	else
 	{
-		_putchar(48+n%10);
-		return (n%10);
+		_putchar(48 + n % 10);
+		return (n % 10);
 	}
 }
