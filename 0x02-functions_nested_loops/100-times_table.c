d90f96f771a7c03358fc473e60e31429188e69dc
Johan <usuario@MacBook-Air-de-Johan-Blandon.local>
2022-02-15 15:19:47 -0500

    This file contain code about 100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * print_times_table - Print times table with the input.
+ * @n: int type input
+ * Return: void function
+ */
+void print_times_table(int n)
+{
+	int n1, n2;
+
+	if (n <=15 && n > 0)
+	{
+		for (n1 = 0; n1 <= n; n1++)
+		{
+			for (n2 = 0; n2 <= n; n2++)
+			{
+				printf("%i", n1 * n2);
+				if (n2 < n)
+				{
+					printf(",");
+					if (n1 * (n2 + 1) < 10)
+						printf("   ");
+					else if (n1 * (n2 + 1) < 100 && n1 * (n2 + 1) >= 10)
+						printf("  ");
+					else
+						printf(" ");
+				}
+			}
+			printf("\n");
+		}
+	}
+	else
+		printf("\n");
+}
