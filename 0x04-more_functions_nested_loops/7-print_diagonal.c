657c6fefe05192a19ba94c93a78fa32240e3e7bc
Johan <usuario@MacBook-Air-de-Johan-Blandon.local>
2022-02-17 15:58:22 -0500

    This file contains info about 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - code print "\" in diagonal.
+ * @n: int type
+ * Return: void.
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (i == j)
+					_putchar('\\');
+				else
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
