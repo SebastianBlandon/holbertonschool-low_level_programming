8a83daf2e2994e8804ea3700990ce6e8e012ee6a
SebastianBlandon <blandonodz@gmail.com>
2022-02-21 17:22:24 -0500

    This file is the 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,10 +7,11 @@
  */
 void print_rev(char *s)
 {
-int i = 0;
-for (i = 0; s[i] != '\0'; i++)
-;
-for (i--; i >= 0; i--)
-_putchar(s[i]);
-_putchar(10);
+	int i = 0;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+		;
+	for (i--; i >= 0; i--)
+		_putchar(*(s + i));
+	_putchar(10);
 }
