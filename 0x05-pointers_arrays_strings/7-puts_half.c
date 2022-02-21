db7d7c5f62df8b08c4d029d74ba60e3cdca104dc
SebastianBlandon <blandonodz@gmail.com>
2022-02-21 16:44:27 -0500

    This file is the 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,11 +9,8 @@ void puts_half(char *str)
 int i = 0, n;
 for (i = 0; str[i] != '\0'; i++)
 ;
-if (i % 2 != 0)
-n = (i - 1) / 2;
-else
-n = i / 2;
-for (; str[i] != '\0'; n++)
+n = i++;
+for (n /= 2; str[i] != '\0'; n++)
 _putchar(str[n]);
 _putchar(10);
 }
