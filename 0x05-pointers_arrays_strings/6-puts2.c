1f3a8e657e8bc5e56a5afd20b53f11cfd4a65eba
SebastianBlandon <blandonodz@gmail.com>
2022-02-21 16:25:00 -0500

    This file is the 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,11 +6,11 @@
  */
 void puts2(char *str)
 {
-int i = 0, j = 0;
+int i = 0;
 while (*(str + i) != '\0')
 {
-_putchar(*(str + j));
-j = j + 2;
+if (i % 2 == 0)
+_putchar(*(str + i));
 i++;
 }
 _putchar(10);
