4bb5216eb6c8d5e24e890eb362f5ddcfa2259474
SebastianBlandon <blandonodz@gmail.com>
2022-02-21 15:23:43 -0500

    This file is the 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * print_rev - print a string reverse form
+ * @s: pointer to char input
+ * Return: void function
+ */
+void print_rev(char *s)
+{
+int i = 0, len = 0, size;
+
+while (*(s + i) != '\0')
+{
+len++;
+i++;
+}
+size = len;
+for (i = 0; i <= size; i++)
+{
+_putchar(*(s + len--));
+}
+_putchar(10);
+}
