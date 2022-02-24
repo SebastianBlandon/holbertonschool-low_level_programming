80f22e8a8d3d4b538886e52ca7bed2347cdbfdf1
SebastianBlandon <blandonodz@gmail.com>
2022-02-23 22:33:18 -0500

    This file contains info about 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *cap_string - capitalize all the words
+ * @a: pointer to char input
+ * Return: char pointer function
+ */
+char *cap_string(char *a)
+{
+	int i = 0, j = 0, new_line = 0, new_space = 0, more_char = 0;
+	char new_l[] = "\n", new_s[] = "\t", more_c[] = ".";
+
+	while (*a)
+	{
+		while (new_l[j])
+		{
+			if (new_l[j] == *a)
+			{
+				new_line = 1;
+			}
+			if (new_s[j] == *a)
+			{
+				new_space = 1;
+			}
+			if (more_c[j] == *a)
+			{
+				more_char = 1;
+			}
+			j++;
+		}
+		if (*a == 32 && *(a + 1) >= 97 && *(a + 1) <= 122)
+			*(a + 1) -= 32;
+		if (new_line && *(a + 1) != '\0')
+			*(a + 1) -= 32;
+		if (new_space)
+		{
+			*a = ' ';
+			*(a + 1) -= 32;
+		}
+		if (more_char && *(a + 1) >= 97 && *(a + 1) <= 122)
+			*(a + 1) -= 32;
+		a++;
+		i++;
+		j = 0;
+		new_line = 0;
+		new_space = 0;
+		more_char = 0;
+	}
+	return (a - i);
+}
