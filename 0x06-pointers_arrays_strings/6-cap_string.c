2b52a5ecb6c9804fd78aa6298532944e19dca5a9
SebastianBlandon <blandonodz@gmail.com>
2022-02-24 13:46:22 -0500

    This file contains info about 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,49 +7,34 @@
  */
 char *cap_string(char *a)
 {
-	int i = 0, j = 0, new_line = 0, new_space = 0, more_char = 0;
-	char new_l[] = "\n", new_s[] = "\t", more_c[] = ",;.!?(){} ";
+	int i = 0, special_character = 0;
 
+	if (a[0] >= 'a' && a[0] <= 'z')
+	{
+		a[0] -= 32;
+	}
 	while (*a)
 	{
-		while (more_c[j])
+		switch (*(a + i))
 		{
-			if (more_c[j] == *a)
-			{
-				more_char = 1;
-			}
-			j++;
+		case ',':
+		case ';':
+		case '.':
+		case '!':
+		case '?':
+		case '(':
+		case ')':
+		case '{':
+		case '}':
+		case '\n':
+		case '\t':
+		case ' ': special_character = 1; break;
 		}
-		j = 0;
-		while (new_l[j])
-		{
-			if (new_l[j] == *a)
-			{
-				new_line = 1;
-			}
-			if (new_s[j] == *a)
-			{
-				new_space = 1;
-			}
-			j++;
-		}
-		if (*a == 32 && *(a + 1) >= 97 && *(a + 1) <= 122)
-			*(a + 1) -= 32;
-		if (new_line && *(a + 1) != '\0')
-			*(a + 1) -= 32;
-		if (new_space)
-		{
-			/*a = ' ';*/
-			*(a + 1) -= 32;
-		}
-		if (more_char && *(a + 1) >= 97 && *(a + 1) <= 122)
-			*(a + 1) -= 32;
+		if (special_character && *(a + 1) >= 97 && *(a + 1) <= 122)
+				*(a + 1) -= 32; 
+		special_character = 0;
 		a++;
 		i++;
-		j = 0;
-		new_line = 0;
-		new_space = 0;
-		more_char = 0;
 	}
 	return (a - i);
 }
