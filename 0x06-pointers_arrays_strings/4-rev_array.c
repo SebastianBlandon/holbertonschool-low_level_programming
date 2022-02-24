f2303e1093dd4f32d3bf0e46a745dadd2cd4481e
SebastianBlandon <blandonodz@gmail.com>
2022-02-24 00:12:05 -0500

    This file contains info about 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,17 +1,23 @@
 #include "main.h"
 #include <stdio.h>
 /**
+ * swap - swap value a, b
+ * @a: pointer to int input
+ * @b: pointer to int input
+ * Return: void function
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+/**
  * reverse_array - reverse arrat integer function
  * @a: pointer to int input
  * @n: int type value
  * Return: void function
  */
-void swap(int* a, int* b)
-{
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
 void reverse_array(int *a, int n)
 {
 	int *p1 = a, *p2 = a + n - 1;
@@ -21,5 +27,5 @@ void reverse_array(int *a, int n)
 		swap(p1, p2);
 		p1++;
 		p2--;
-	} 
+	}
 }
