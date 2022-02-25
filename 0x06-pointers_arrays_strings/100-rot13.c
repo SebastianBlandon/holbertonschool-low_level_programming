760ac615a70ec8a9cfddc89f9aeeb7ae5a147cd2
SebastianBlandon <blandonodz@gmail.com>
2022-02-25 00:04:55 -0500

    This file contains info about 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,16 +6,19 @@
  */
 char *rot13(char *a)
 {
-	int i;
-	char upper[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
-	char lower[] = "nopqrstuvwxyzabcdefghijklm";
+	int i, j;
+	char alph[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
 	for (i = 0; *(a + i) != '\0'; i++)
 	{
-		if ((*(a + i) > 64 && *(a + i) < 91) || (*(a + i) > 96 && *(a + i) <= 123))
+		for (j = 0; *(alph + j) != '\0'; j++)
 		{
-			*(a + i) = (*(a + i) - 65 > 25) ?
-				lower[*(a + i) - 97] : upper[*(a + i) - 65];
+			if (*(a + i) == *(alph + j))
+			{
+				*(a + i) = *(rot + j);
+				break;
+			}
 		}
 	}
 	return (a);
