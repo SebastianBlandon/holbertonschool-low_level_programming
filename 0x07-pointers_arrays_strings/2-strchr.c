649c33f1553775a0c077cbc25528a7f297c1426f
SebastianBlandon <blandonodz@gmail.com>
2022-02-28 12:31:35 -0500

    This file contains info about function 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,7 +13,7 @@ char *_strchr(char *s, char c)
 	for (i = 0; *(s + i) != '\0'; i++)
 		if (*(s + i) == c)
 			return (s + i);
-		else if (*(s + i + 1) == c)
-			return (s + i + 1);
-	return (s + i);
+	if (*(s + i) == c)
+		return (s + i);
+	return ('\0');
 }
