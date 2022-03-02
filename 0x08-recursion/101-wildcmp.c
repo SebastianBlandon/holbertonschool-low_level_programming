d023655a6bda76a4ae212c115e5524c522c03c57
SebastianBlandon <blandonodz@gmail.com>
2022-03-02 15:20:02 -0500

    This file contains info about funtion 101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * wildcmp - compares two strings and returns 1 
+ * if the strings can be considered identical, otherwise return 0.
+ * @s1: pointer char input
+ * @s2: pointer char input
+ * Return: int value output
+ */
+int wildcmp(char *s1, char *s2)
+{
+	
+}
