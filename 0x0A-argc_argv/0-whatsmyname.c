074e28c9e9ecc894b43578958090552b76b0a5bd
SebastianBlandon <blandonodz@gmail.com>
2022-03-04 11:40:49 -0500

    This file contains info about function 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - print the name executable
+ * main - prints its name, followed by a new line.
  * @argc: int variable input, size argv
  * @argv: pointer to char array input, contain arguments
  * Return: 0 always
