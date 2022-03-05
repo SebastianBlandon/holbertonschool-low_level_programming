8c7ddf1651ab6790d6c4cad47bec8ceaad8d6913
SebastianBlandon <blandonodz@gmail.com>
2022-03-05 14:27:21 -0500

    This file contain info about 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,43 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-/**
- * count_cents - count the coins
- * @amount: int variable input
- * @cents: int variable input
- * Return: cents unsigned int output
- */
-long count_cents(int amount, int cents)
-{
-	if (!amount)
-		return (cents);
-	if (!(amount % 25))
-	{
-		amount -= 25;
-		cents++;
-	}
-	else if (!(amount % 10))
-	{
-		amount -= 10;
-		cents++;
-	}
-	else if (!(amount % 5))
-	{
-		amount -= 5;
-		cents++;
-	}
-	else if (!(amount % 2))
-	{
-		amount -= 2;
-		cents++;
-	}
-	else if (!(amount % 1))
-	{
-		amount -= 1;
-		cents++;
-	}
-	return (count_cents(amount, cents));
-}
 /**
  * main - prints the minimum number of coins to make change
  *  for an amount of money.
@@ -47,6 +9,7 @@ long count_cents(int amount, int cents)
  */
 int main(int argc, char *argv[])
 {
+	int amount, cents = 0;
 	if (argc == 1 || argc > 2)
 	{
 		printf("Error\n");
@@ -54,7 +17,29 @@ int main(int argc, char *argv[])
 	}
 
 	if (atoi(argv[argc - 1]) > 0)
-		printf("%li\n", count_cents(atoi(argv[argc - 1]), 0));
+	{
+		amount = atoi(argv[argc - 1]);
+		while (amount)
+		{
+			if ((amount >= 25))
+				amount -= 25;
+
+			else if ((amount >= 10))
+				amount -= 10;
+
+			else if ((amount >= 5))
+				amount -= 5;
+
+			else if ((amount >= 2))
+				amount -= 2;
+
+			else if ((amount >= 1))
+				amount -= 1;
+
+			cents++;
+		}
+		printf("%d\n", cents);
+	}
 
 	else
 		printf("0\n");
