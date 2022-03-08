1bbf4f14f8f2796524133cd9a6d14223cccf9aef
SebastianBlandon <blandonodz@gmail.com>
2022-03-08 10:58:35 -0500

    This file contains info about 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -66,6 +66,7 @@ char **strtow(char *str)
 		return (NULL);
 
 	amount_word_str = _stramount_word(str);
+	printf("%d\n", amount_word_str);
 	size = _strlen(str);
 	str1 = malloc(size * sizeof(*str1));
 	if (!str1)
