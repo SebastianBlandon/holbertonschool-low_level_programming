9b6ef1fe263861d3cf1b6ff8edbba1d48ed7bc1a
SebastianBlandon <blandonodz@gmail.com>
2022-03-10 16:19:20 -0500

    This file contains info about 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,20 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
-/**
- * _strlen - calculate the lengt of string
- * @s: pointer to char input
- * Return: len (Size s).
- */
-unsigned long int _strlen(int *s)
-{
-	unsigned long int len = 0;
-
-	while (*(s + len) != '\0')
-		len++;
-	return (len);
-}
 /**
  * main - multiplies two numbers.
  * @argc: int variable input, size argv
@@ -36,18 +21,18 @@ int main(int argc, char *argv[])
 		multiple_10 /= 10;
 		while (multiple_10 != 1)
 		{
-			_putchar(mul/multiple_10 + 48);
-			mul -= (multiple_10 * (mul/multiple_10));
+			_putchar(mul / multiple_10 + 48);
+			mul -= (multiple_10 * (mul / multiple_10));
 			multiple_10 /= 10;
 		}
-		_putchar(mul%10 + 48);
+		_putchar(mul % 10 + 48);
 		_putchar(10);
 	}
 	else
 	{
 		_putchar('E'), _putchar('r'), _putchar('r'), _putchar('o'), _putchar('r');
 		_putchar('\n');
-		exit (98);
+		exit(98);
 	}
 	return (0);
 }
