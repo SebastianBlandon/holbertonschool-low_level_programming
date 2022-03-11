5c82fbdde0a932d5729c2815ed7f78c9d9543eec
SebastianBlandon <blandonodz@gmail.com>
2022-03-11 12:32:12 -0500

    This file contains info about 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -18,7 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb <= 0 || size <= 0)
 		return (NULL);
 
-	alloc = malloc(nmemb * size);
+	alloc = malloc(nmemb * size + 1);
 	if (!alloc)
 		return (NULL);
 
