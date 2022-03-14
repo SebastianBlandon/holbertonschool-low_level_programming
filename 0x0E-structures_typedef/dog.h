663b486acf7e85f4113ec0a10ea7f303ee1a937d
SebastianBlandon <blandonodz@gmail.com>
2022-03-14 11:05:44 -0500

    This file contains info about dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - dog struct type
+ * @name: Name dog
+ * @age: Age dog
+ * @owner: Owner dog
+ *
+ * Description: Structure for any type of dog
+ */
+struct dog
+{
+	char * name;
+	float age;
+	char * owner;
+};
