d968966559c531b9b7e9ecd28021ae4a788d1928
SebastianBlandon <blandonodz@gmail.com>
2022-03-14 12:20:47 -0500

    This file contains info about 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,6 @@
 #ifndef _FUNCTION_LIKE_MACRO_H_
 #define _FUNCTION_LIKE_MACRO_H_
 
-#include <stdlib.h>
-#define ABS(x) abs(x)
+#define ABS(x) (x > 0 ? (x) : (x) * -1) /*Ternary Operator*/
 
 #endif /* _FUNCTION_LIKE_MACRO_H_ */
