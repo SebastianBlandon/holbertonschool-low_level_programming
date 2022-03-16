44fa4d74745bfce3e830126e44092f401741f7a3
SebastianBlandon <blandonodz@gmail.com>
2022-03-16 11:13:14 -0500

    This file contains info about 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - print name using the pointer to void function
+ * @name: pointer to int input
+ * @f: pointer to void function input
+ * Return: void function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
