b6b4f26c9f3a5483ae38bd10277ee9473b4e63fe
SebastianBlandon <blandonodz@gmail.com>
2022-03-17 00:34:49 -0500

    This file contains info about functions_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
-#ifndef FUNCTION_POINTERS_H_   /* Include guard */
+#ifndef FUNCTION_POINTERS_H_
 #define FUNCTION_POINTERS_H_
 
-#include <stddef.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
