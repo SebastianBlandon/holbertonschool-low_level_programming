badf4f9dacb79450cf289368ec51ee1da7f58a13
SebastianBlandon <blandonodz@gmail.com>
2022-04-01 10:48:29 -0500

    10-delete_nodeint.c review case NULL v1.2

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *tmp, *aux = *head;
 	unsigned int i = 0;
 
-	if (!head)
+	if (!head || !aux)
 		return (-1);
 
 	if (index == 0)
