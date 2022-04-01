8dd29b4708ea20cc24b3ecd4b87388a7aa273ed9
SebastianBlandon <blandonodz@gmail.com>
2022-04-01 10:37:25 -0500

    9-insert_nodeint.c review v1.3

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -41,7 +41,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	aux = (aux)->next;
 	new->n = n;
-	new->next = aux;
-	tmp->next = new;
+	if (aux)
+	{
+		new->next = aux;
+		tmp->next = new;
+	}
+	else
+		new->next = NULL;
+		tmp->next = new;
 	return (new);
 }
