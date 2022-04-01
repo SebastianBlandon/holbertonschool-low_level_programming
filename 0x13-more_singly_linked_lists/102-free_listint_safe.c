a269505c0b7a6fe7ea124607db0f10698a07caa8
SebastianBlandon <blandonodz@gmail.com>
2022-03-31 22:10:47 -0500

    102-free_listint_safe.c ready version 1.0

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees a listint_t linked list.
+ * @h: listint_t double pointer input
+ * Return: the size of the list that was free’d
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *current = *h, *aux;
+	size_t nodes = 0;
+
+	if (!current)
+		return (-1);
+
+	while (current)
+	{
+		aux = current;
+		current = current->next;
+		nodes++;
+
+		if (current > aux)
+			break;
+		free(aux);
+	}
+	*h = NULL;
+	return (nodes);
+}
