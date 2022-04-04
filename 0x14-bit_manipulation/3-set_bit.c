2f4e8f965d7df221773d2a769888d61a0f88d608
SebastianBlandon <blandonodz@gmail.com>
2022-04-04 17:34:46 -0500

    3-set_bit.c ready version 1.0

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_bit - sets the value of a bit to 1 at a given index.
+ * @n: unsigned long int input
+ * @index: unsigned int input
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 64)
+		return (-1);
+
+	*n >>= index;
+	*n += 1;
+	*n <<= index;
+	return (1);
+}
