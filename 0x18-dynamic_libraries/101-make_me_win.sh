202c101d48bb9e87d47faa34a0fb68ab3d1e1533
SebastianBlandon <blandonodz@gmail.com>
2022-05-16 21:54:46 -0700

    [UPDATED] 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /root/ https://github.com/SebastianBlandon/holbertonschool-low_level_programming/blob/main/0x18-dynamic_libraries/librandom.so
-export LD_PRELOAD=/root/librandom.so
+wget -P /tmp/ https://github.com/SebastianBlandon/holbertonschool-low_level_programming/blob/main/0x18-dynamic_libraries/librandom.so
+export LD_PRELOAD=/tmp/librandom.so
