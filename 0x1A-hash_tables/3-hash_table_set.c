d4142c55c0c209f144924262ebe9ee4127579eda
SebastianBlandon <blandonodz@gmail.com>
2022-06-05 15:06:58 -0500

    [UPDATED] 3-hash_table_set.c, fix copy key and value

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -24,7 +24,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			if (!strcmp(key, tmp->key))
 			{
-				strcpy(tmp->value, value);
+				tmp->value = strdup(value);
 				return (1);
 			}
 			tmp = tmp->next;
@@ -35,8 +35,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!new_node)
 		return (0);
 
-	strcpy(new_node->key, key);
-	strcpy(new_node->value, value);
+	new_node->key = strdup(key);	
+	new_node->value = strdup(value);
 	new_node->next = NULL;
 
 	if (ht->array[index])
