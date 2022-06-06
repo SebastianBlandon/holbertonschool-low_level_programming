d68283f3cda459bc72cbe1416b38a4b0b669f67a
SebastianBlandon <blandonodz@gmail.com>
2022-06-06 16:35:17 -0500

    [UPDATED] 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,22 +18,22 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	index = key_index((const unsigned char *)key, ht->size);
 	tmp = ht->array[index];
-	if (tmp)
+	while (tmp)
 	{
-		while (tmp)
+		if (!strcmp(key, tmp->key))
 		{
-			if (!strcmp(key, tmp->key))
-			{
-				tmp->value = strdup(value);
-				return (1);
-			}
-			tmp = tmp->next;
+			tmp->value = strdup(value);
+			return (1);
 		}
+		tmp = tmp->next;
 	}
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
+	{
+		free(new_node);
 		return (0);
+	}
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
 	new_node->next = ht->array[index];
